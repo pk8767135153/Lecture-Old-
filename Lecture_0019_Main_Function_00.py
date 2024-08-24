9a9b5ac2bac3b9607b52f5058de63e01416c9b0d
Pravinsingh Korekar <123065607+pk8767135153@users.noreply.github.com>
2024-08-24 22:12:36 +0530

    Basic Lecture Code__ Date 24_08_2024
    Learning Purpose

---
+++ Lecture_0019_Main_Function_00.py
@@ -0,0 +1,7 @@
+import Lecture_0019_Main_Function_02 as fun2
+
+print(fun2.add(8,1))
+
+'''
+        In this code we import Lecture_0019_Main_Function_02.py file which import all ready in another file that file name is Lecture_0019_Main_Function_01.py in _01 file we can print the add method but it will run all the child file
+'''
---
+++ Lecture_0019_Main_Function_01.py
@@ -0,0 +1,3 @@
+import Lecture_0019_Main_Function_02 as fun2
+
+print(fun2.add(9,9))
